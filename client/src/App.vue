89a25bd91394c14798ff8ad921afcca3ae2b273d
Avyukt Soni <soniavyukt@gmail.com>
2024-05-15 17:26:27 +0530

    init

---
+++ client/src/App.vue
@@ -0,0 +1,49 @@
+<!-- <template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import AuthPage from "./components/Auth.vue";
+
+export default {
+  name: "App",
+  components: {
+    AuthPage,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style> -->
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
