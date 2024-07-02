8d75a4ffa6f97a178d821d6a7586a82742235c8c
Avyukt Soni <soniavyukt@gmail.com>
2024-07-02 19:15:44 +0530

    landing page init

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,14 @@
 import CryptoNav from "../components/navbar";
 // import Homepage from "./pages/homepage";
 import { Dashboard } from "@/components/dashboard";
+import { Landing } from "@/components/landing";
 
 export default function Home() {
   return (
-    <>
+    <main className="">
       {/* <CryptoNav /> */}
-      <Dashboard />
-    </>
+      {/* <Dashboard /> */}
+      <Landing />
+    </main>
   );
 }
