c161dd56c77188e4e0505e8230d46e89092f8087
Avyukt Soni <soniavyukt@gmail.com>
2024-07-21 16:53:48 +0530

    port changes

--- server/app.py
+++ server/app.py
@@ -142,4 +142,6 @@ def delete_file():
         return jsonify({'status': 'error', 'message': str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    # app.run(debug=True, port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
